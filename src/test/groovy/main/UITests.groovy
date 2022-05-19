d55cd8f6ac07f7e44b333f42e16b0974b9972b30
mike-duvall <mike.a.duvall@gmail.com>
2022-05-19 07:23:08 -0500

    Refactor of existing test

--- src/test/groovy/main/UITests.groovy
+++ src/test/groovy/main/UITests.groovy
@@ -6,7 +6,7 @@ import client.MikeAndConquerUIClient
 import domain.Unit
 import groovy.json.JsonSlurper
 import domain.SimulationStateUpdateEvent
-
+import org.junit.Test
 import spock.lang.Specification
 import util.TestUtil
 import util.Util
@@ -52,6 +52,58 @@ class UITests extends Specification {
 
 
         when:
+        minigunnerId = TestUtil.assertMinigunnerCreatedEventReceived(simulationClient)
+
+        then:
+        assert minigunnerId != -1
+
+        when:
+        uiClient.selectUnit(minigunnerId)
+
+        then:
+        TestUtil.assertUnitIsSelected(uiClient, minigunnerId)
+
+        when:
+        uiClient.leftClickInMapSquareCoordinates(18,12)
+        Point destinationAsWorldCoordinates = Util.convertMapSquareCoordinatesToWorldCoordinates(18,12)
+        int destinationXInWorldCoordinates = destinationAsWorldCoordinates.x
+        int destinationYInWorldCoordinates = destinationAsWorldCoordinates.y
+
+
+        and:
+        int expectedTotalEvents = 46
+
+        and:
+        TestUtil.assertNumberOfSimulationStateUpdateEvents(simulationClient,expectedTotalEvents)
+
+        then:
+        List<SimulationStateUpdateEvent> gameEventList = simulationClient.getSimulationStateUpdateEvents()
+        SimulationStateUpdateEvent expectedUnitOrderedToMoveEvent = gameEventList.get(2)
+        TestUtil.assertUnitOrderedToMoveEvent(expectedUnitOrderedToMoveEvent, minigunnerId, destinationXInWorldCoordinates, destinationYInWorldCoordinates)
+
+
+        and:
+        SimulationStateUpdateEvent expectedUnitArrivedAtDestinationEvent = gameEventList.get(expectedTotalEvents - 1)
+        TestUtil.assertUnitArrivedAtDestinationEvent(expectedUnitArrivedAtDestinationEvent, minigunnerId)
+
+
+
+    }
+
+    def "Move a minigunner and validate path"() {
+        given:
+        def jsonSlurper = new JsonSlurper()
+        uiClient.startScenario()
+        int minigunnerId = -1
+
+        when:
+        simulationClient.addGDIMinigunnerAtMapSquare(18,14)
+
+        then:
+        TestUtil.assertNumberOfSimulationStateUpdateEvents(simulationClient, 2)
+
+
+        when:
         List<SimulationStateUpdateEvent> events = simulationClient.getSimulationStateUpdateEvents()
         for(event in events) {
             if (event.eventType == "MinigunnerCreated") {
@@ -68,15 +120,6 @@ class UITests extends Specification {
         sleep(1000)
         uiClient.selectUnit(minigunnerId)
 
-//        then:
-//        true
-//        println("i=" + i)
-
-//        where:
-//        i << (1..1)
-
-//        then:
-//        true
         and:
         sleep(4000)
 
@@ -94,7 +137,7 @@ class UITests extends Specification {
 
         and:
         sleep(5000)
-        int expectedTotalEvents = 46
+        int expectedTotalEvents = 47
 
         and:
         List<SimulationStateUpdateEvent> gameEventList = simulationClient.getSimulationStateUpdateEvents()
@@ -108,6 +151,16 @@ class UITests extends Specification {
         assert secondEventDataAsObject.DestinationXInWorldCoordinates == destinationXInWorldCoordinates
         assert secondEventDataAsObject.DestinationYInWorldCoordinates == destinationYInWorldCoordinates
         assert secondEventDataAsObject.UnitId == minigunnerId
+
+
+//        and:
+//        SimulationStateUpdateEvent thirdEvent = gameEventList.get(3)
+//        Pickup here
+        // assert "UnitPlansMovementPath" event (or something like that)
+        // assert that actual path, in the for of a list of maptiles, is the correct path
+
+        // Then assert that the unit starts moving and passes through every map tile in the path, in order
+
 
         and:
         SimulationStateUpdateEvent thirdEvent = gameEventList.get(expectedTotalEvents - 1)
@@ -126,5 +179,7 @@ class UITests extends Specification {
 
 
     }
+
+
 
 }
